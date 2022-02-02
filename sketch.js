ab70493b10ba7dc2018e4f8dd14a4f1add2ae882
jdeboi <jdeboi@gmail.com>
2022-02-02 13:50:28 -0800

    first commit

---
+++ sketch.js
@@ -0,0 +1,111 @@
+/*
+* p5.mapper
+* Gif on quad surface
+* 
+* Jenna deBoisblanc
+* jdeboi.com
+* 11/16/2021
+* 
+*/
+
+let pMapper;
+let quadMap;
+
+let gifs = [];
+let isPlaying = false;
+
+let myFont;
+let currentIndex = 0;
+
+function preload() {
+    myFont = loadFont('assets/Roboto.ttf');
+    // for (let i = 0; i < 12; i++) {
+    //     gifs[i] = loadImage("assets/" +  i + ".gif");
+    // }
+    gifs[0] = loadImage("assets/" +  6 + ".gif");
+    // video.hide();
+}
+
+function change() {
+    if (frameCount % (60*4) === 0) {
+        currentIndex++;
+        currentIndex %= gifs.length;
+        console.log(currentIndex)
+    }
+}
+function setup() {
+    createCanvas(windowWidth, windowHeight, WEBGL);
+
+    textFont(myFont);
+
+    pMapper = createProjectionMapper(this);
+    quadMap = pMapper.createQuadMap(gifs[0].width, gifs[0].height);
+    pMapper.load("maps/map.json");
+
+    for (const gif of gifs) {
+        gif.play();
+    }
+    noCursor();
+}
+
+function draw() {
+    background(0);
+
+    // displayFrameRate();
+
+    // quadMap.clear();
+    // quadMap.translate(-quadMap.width / 2, -quadMap.height / 2);
+
+    // quadMap.image(gif, 0, 0);
+    // quadMap.noFill();
+
+    translate(-width/2, -height/2)
+    image(gifs[currentIndex], 0, 0, width, height);
+
+    // change();
+}
+
+function keyPressed() {
+    switch (key) {
+        case 'c':
+            pMapper.toggleCalibration();
+            break;
+        case 'f':
+            let fs = fullscreen();
+            document.getElementById("header").style.display = "none";
+            fullscreen(!fs);
+            break;
+        case 'l':
+            pMapper.load("maps/map.json");
+            break;
+
+        case 's':
+            pMapper.save("map.json");
+            break;
+    }
+}
+
+function mousePressed() {
+    isPlaying = true;
+    // video.play();
+    // gifs.play();
+    pMapper.onClick();
+}
+
+function mouseDragged() {
+    pMapper.onDrag();
+}
+
+function mouseReleased() {
+    pMapper.onRelease();
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function displayFrameRate() {
+    fill(255);
+    noStroke();
+    text(round(frameRate()), -width / 2 + 20, -height / 2 + 20);
+}
